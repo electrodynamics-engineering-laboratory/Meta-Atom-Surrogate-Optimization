6a8ffe95db7d5bd051c7c7bb90c27c8a96755bec
JosephHaun <riddlemethis565@gmail.com>
2019-06-26 12:42:16 -0700

    Created GUI function file

---
+++ ElectrodynamicsEngineerLaboratorySurrogateGUI.m
@@ -0,0 +1,12 @@
+function ElectrodynamicsEngineerLaboratorySurrogateGUI()
+%{
+    The function creates the GUI that will allow normal users to perform
+    surrogate operations and view the results obtained in graphical format.
+
+    Author: Joseph Haun
+    Date: June 26, 2019
+
+    
+%}
+
+end
