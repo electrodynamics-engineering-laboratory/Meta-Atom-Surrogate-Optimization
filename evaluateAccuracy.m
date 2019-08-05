b8402dbd9ad813241bd1dc7cf916747aca4648fa
JosephHaun <riddlemethis565@gmail.com>
2019-08-05 13:34:55 -0700

    Updated the accuracy evaluation function to display graphs of each data column which represents the specific value

--- evaluateAccuracy.m
+++ evaluateAccuracy.m
@@ -1,13 +1,14 @@
-function [Figures] = evaluateAccuracy(EELMetamodelOutput)
+function [Output] = evaluateAccuracy(EELMetamodelOutput)
 %{
 
 %}
 
 Input = EELMetamodelOutput;
+Output = struct('TestPointEval', [], 'GivenYSValues', [], 'NormalizedYS', [], 'Figures', []);
 
 for k = 1:size(Input.ys,2)
 
-InputYS = Input.ys(:,k);
+InputYS = transpose(Input.ys(:,k));
 InputParameters = Input.parameters(:,k);
 
 TestPointLength = size(InputParameters.TestPoints,1);
@@ -17,9 +18,26 @@ for i = 1:TestPointLength
     TestPointEval(i) = feval(InputParameters.objectFunction, InputParameters.TestPoints(i,:));
 end
 
-TestPointEval
+NormalizedYSValues = InputYS./TestPointEval;
+
+Output.TestPointEval = [Output.TestPointEval; TestPointEval];
+Output.GivenYSValues = [Output.GivenYSValues; InputYS];
+Output.NormalizedYS = [Output.NormalizedYS; NormalizedYSValues];
 
 end
 
+for k = 1:size(Input.ys,1)
+   TempFigure = figure();
+   plot([1:length(Output.GivenYSValues(:,k))], Output.GivenYSValues(:,k), 'ro')
+   hold on
+   plot([1:length(Output.NormalizedYS(:,k))], Output.TestPointEval(:,k), 'bx')
+   hold off
+   legend('Given YS Values', 'Test Point Evaluations', 'Location', 'southoutside')
+   title(strcat("X",string(k)));
+   xlabel("Evaluation Number");
+   ylabel(strcat("Value"));
+   Output.Figures = [Output.Figures TempFigure];
+   
+end
 
 end
