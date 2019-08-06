76b9f698952afd8e75afac5c9b4cb6908a8b49ae
JosephHaun <riddlemethis565@gmail.com>
2019-08-06 11:00:43 -0700

    Function puts normalized ys, given ys, and evaluated data point values on single graph

--- evaluateAccuracy.m
+++ evaluateAccuracy.m
@@ -1,10 +1,41 @@
-function [Output] = evaluateAccuracy(EELMetamodelOutput)
+function [AccuracyOutput] = evaluateAccuracy(EELMetamodelOutput)
 %{
+    This function takes in the output of the runModelTest function and
+    creates graphs of the ys values and the object function evaluated at
+    the test points
 
+    Input: EELMetamodelOutput - an array of structures containing the
+                                fields: ys, dmodel, error, and parameters
+                    
+                    ys - an m-by-n matrix of values where each column
+                    corresponds to a dimension of the problem and each row
+                    corresponds to a specific run of the get_ys_krig
+                    function
+                    dmodel - an array of structures that is the output of 
+                    the dacefit function which contains various parameters 
+                    used to create a metamodel
+                    error - an array that contains any error messages
+                    output by the runModelTest function
+                    parameters - an array of structures that contains the
+                    values input to the get_ys_krig function within the
+                    EELMetamodel function
+    
+    Output: AccuracyOutput - an array of structures containing the fields:
+                    TestPointEval, GivenYSValues, NormalizedYS, and Figures
+                    
+                    TestPointEval - a m-by-n matrix of values created by
+                    evaluating the given object function at each testing
+                    point given
+                    GivenYSValues - an m-by-n matrix of values that are
+                    given to the function
+                    NormalizedYS - an m-by-n matrix of values that are each
+                    element of the GivenYSValues divided by each element of
+                    the TestPointEval
+                    
 %}
 
 Input = EELMetamodelOutput;
-Output = struct('TestPointEval', [], 'GivenYSValues', [], 'NormalizedYS', [], 'Figures', []);
+AccuracyOutput = struct('TestPointEval', [], 'GivenYSValues', [], 'NormalizedYS', [], 'Figures', []);
 
 for k = 1:size(Input.ys,2)
 
@@ -20,23 +51,25 @@ end
 
 NormalizedYSValues = InputYS./TestPointEval;
 
-Output.TestPointEval = [Output.TestPointEval; TestPointEval];
-Output.GivenYSValues = [Output.GivenYSValues; InputYS];
-Output.NormalizedYS = [Output.NormalizedYS; NormalizedYSValues];
+AccuracyOutput.TestPointEval = [AccuracyOutput.TestPointEval; TestPointEval];
+AccuracyOutput.GivenYSValues = [AccuracyOutput.GivenYSValues; InputYS];
+AccuracyOutput.NormalizedYS = [AccuracyOutput.NormalizedYS; NormalizedYSValues];
 
 end
 
 for k = 1:size(Input.ys,1)
    TempFigure = figure();
-   plot([1:length(Output.GivenYSValues(:,k))], Output.GivenYSValues(:,k), 'ro')
+   plot([1:length(AccuracyOutput.GivenYSValues(:,k))], AccuracyOutput.GivenYSValues(:,k), 'ro')
    hold on
-   plot([1:length(Output.NormalizedYS(:,k))], Output.TestPointEval(:,k), 'bx')
+   plot([1:length(AccuracyOutput.NormalizedYS(:,k))], AccuracyOutput.TestPointEval(:,k), 'bx')
+   plot([1:length(AccuracyOutput.NormalizedYS(:,k))], AccuracyOutput.NormalizedYS(:,k), 'ks')
    hold off
-   legend('Given YS Values', 'Test Point Evaluations', 'Location', 'southoutside')
-   title(strcat("X",string(k)));
+   legend('Given YS Values', 'Test Point Evaluations', 'Normalized YS Values', 'Location', 'southoutside')
+   title(strcat("Dimension Parameter (",string(k),")"));
    xlabel("Evaluation Number");
    ylabel(strcat("Value"));
-   Output.Figures = [Output.Figures TempFigure];
+   AccuracyOutput.Figures = [AccuracyOutput.Figures TempFigure];
+   pause(0.5)
    
 end
 
