23d8793ab625a65fadef1543dc2908bd7465c4a0
JosephHaun <riddlemethis565@gmail.com>
2019-09-16 15:56:17 -0700

    Resetting head

--- RMSEScript.m
+++ RMSEScript.m
@@ -1,68 +1,69 @@
 clear;
-TargetPath = "TestingOutputs";
+TargetPath = "TestingOutputs/";
 TargetDirectory = dir(TargetPath);
-ValidFiles = string(1:length(TargetDirectory));
+ValidFiles = string(1:(length(TargetDirectory)-3));
 
+minStringIndex = 10;
+maxStringIndex = 13;
 for i = 1:length(TargetDirectory)
-    if(~strcmp(TargetDirectory(i).name, '.') & ~strcmp(TargetDirectory(i).name,'..') & ~strcmp(TargetDirectory(i).name, 'Old_Runs'))
-        ValidFiles(i) = strcat(TargetPath, "/", string(TargetDirectory(i).name));
-    else
-       ValidFiles(i) = "000"; 
+    for j = 0:(maxStringIndex-minStringIndex)
+        if(~isfolder(strcat(TargetPath,TargetDirectory(i).name)))    
+            tempName = TargetDirectory(i).name(minStringIndex:(maxStringIndex-j));
+            Position = find(ValidFiles == string(tempName));
+            if(~isempty(Position))
+               ValidFiles(Position) = strcat(TargetPath,TargetDirectory(i).name);
+               break; 
+            end
+        end
     end
 end
 
-OutputArray = struct('ys', [], 'dmodel', [], 'error', [], 'parameters', []);
+RMSEValues = zeros(length(ValidFiles), 18);
+TrainingPointLengths = zeros(length(ValidFiles),18);
+Correlation = zeros(length(ValidFiles),18);
+RegressionPolynomial = zeros(length(ValidFiles),18);
 
+%Get TrainingPoint length
  for i = 1:length(ValidFiles)
-     try
-        load(ValidFiles(i)); 
-        OutputArray = [OutputArray Output];
-     catch ME
-         display(ME.message)
-     end
- end
- 
- %Calculate RMSE Values and Graph
- RMSEValues = [];
- TrainingPointLength = [];
- 
- %Get TrainingPoint lengths
- for i = 2:length(OutputArray)
-     InternalRMSEValues = 1:size(OutputArray(i).ys,2);
-     InternalTrainingPointLength = 1:size(OutputArray(i).ys,2);
-     for j = 1:size(OutputArray(i).ys,2)
-        InternalYS = transpose(OutputArray(i).ys(:,j));
-        InternalParam = OutputArray(i).parameters;
+     load(ValidFiles(i))
+     InternalRMSEValues = zeros(1, size(Output.ys,2));
+     InternalTrainingPointLength = zeros(1,size(Output.ys,2));
+     InternalCorrelation = zeros(1,size(Output.ys,2));
+     InternalRegrPolynomial = zeros(1,size(Output.ys,2));
+     
+     for j = 1:size(Output.ys,2)
+        InternalYS = transpose(Output.ys(:,j));
+        InternalParam = Output.parameters;
      
         TestPointLength = size(InternalParam(j).TestPoints,1);
         InternalYSEval = 1:TestPointLength;
         TestPointEval = 1:TestPointLength;
      
         for k = 1:TestPointLength
+            InternalParam(j).TestPoints(k,:);
             TestPointEval(k) = feval(InternalParam(j).objectFunction, InternalParam(j).TestPoints(k,:));
         end
-     
-        InternalTrainingPointLength(j) = InternalParam.numStartPoints;
-        
-        if(InternalParam(j).numStartPoints > 50)
-            display("TESTING")
-        end
-        
+        InternalCorrelation(j) = InternalParam(j).correlation;
+        InternalRegrPolynomial(j) = InternalParam(j).regressionPolynomial;
+        InternalTrainingPointLength(j) = InternalParam(j).numStartPoints;
         InternalRMSEValues(j) = getRMSE(InternalYS, TestPointEval);
      end
-     if(length(InternalRMSEValues) == 6) %Pad with zeros due to some error happening below five training points
-        InternalRMSEValues = [InternalRMSEValues zeros(1,6)];
-     end
-     RMSEValues = [RMSEValues; InternalRMSEValues];
-     if(length(InternalTrainingPointLength) == 6) %Pad with zeros due to some error happening below five training points
-         InternalTrainingPointLength = [InternalTrainingPointLength zeros(1,6)];
-     end
-     TrainingPointLength = [TrainingPointLength; InternalTrainingPointLength];
+     RMSEValues(i,:) = InternalRMSEValues;
+     TrainingPointLengths(i,:) = InternalTrainingPointLength;
+     RegressionPolynomial(i,:) = InternalRegrPolynomial;
+     Correlation(i,:) = InternalCorrelation;
  end
- 
- Figure1 = figure();
- %subplot(2,1,1)
- plot(TrainingPointLength(:,1), RMSEValues(:,1), 'rx')
- xlabel("Training Point Length")
- ylabel("re(RMSE)")
- title("re(RMSE) vs. Training Point Length")
+ ParamPairs = ["Cubic-Regr0", "Exponential-Regr0", "Gaussian-Regr0", "Linear-Regr0", "Spherical-Regr0", "Spline-Regr0" ...
+       "Cubic-Regr1", "Exponential-Regr1", "Gaussian-Regr1", "Linear-Regr1", "Spherical-Regr1", "Spline-Regr1" ...
+       "Cubic-Regr2", "Exponential-Regr2", "Gaussian-Regr2", "Linear-Regr2", "Spherical-Regr2", "Spline-Regr2"];
+ Figures = [];
+ for i = 1:size(RMSEValues,2)
+     Figure = [Figures figure()];
+     %subplot(2,1,1)
+     plot(TrainingPointLengths(:,i), RMSEValues(:,i), 'rx')
+     ylim([0, 0.25])
+     xlim([0, (length(TargetDirectory)-3)])
+     xlabel("Training Point Length")
+     ylabel("re(RMSE)")
+     title(strcat("re(RMSE) vs. Training Point Length: ", ParamPairs(i)))
+ end
--- runModelTest.m
+++ runModelTest.m
@@ -11,7 +11,7 @@ if nargin == 0
     inputStartPoints = 1; %If no number is given, assume one test is to be run
 end
 
-for numStartPoints = 1:inputStartPoints  
+for numStartPoints = (1):(inputStartPoints)  
     
     testsToRun = 1; %Only run the specific parameters once
     func = @(x)real((x(:,3)+i*x(:,4))./(x(:,1)+i*x(:,2) + x(:,3)+i*x(:,4))); %The function to be tested against
@@ -30,17 +30,28 @@ for numStartPoints = 1:inputStartPoints
     %Preallocate a blank structure for data input later
     Output = struct('ys', [], 'dmodel', [], 'error', [], 'parameters', []);
 
+    %Preallocate dummy structures in case of error, this separates outputs
+    %for later parsing. 
+    tempDModel = struct('regr', NaN, 'corr', NaN, 'theta', NaN, 'beta', NaN, 'gamma', NaN, 'sigma2', NaN, 'S', NaN, 'Ssc', NaN, 'Ysc', NaN, 'C', NaN, 'Ft', NaN, 'G', NaN);
+    tempParameters = struct('TrainingPoints', NaN, 'FunctionValues', NaN, 'TestPoints', NaN*zeros(4), 'dimension', NaN, 'numStartPoints', NaN, 'initialDesign', NaN, 'xLim', NaN, 'objectFunction', func, 'thetaBound', NaN, 'correlation', NaN, 'regressionPolynomial', NaN);
+    tempYs = NaN*zeros(dimension,1);           
+    
     for Iterations = 1:testsToRun
         for IDChoice = 1:length(initDesign)
            for SSChoice = 1:length(sampStrat)
               for RPChoice = 1:length(regrPoly)
                   try
+                    tempStruct = struct('initDesign', initDesign(IDChoice), 'sampStrat', sampStrat(SSChoice), 'regrPoly', regrPoly(RPChoice), 'message', 'NONE');
                     [ys, dmodel, parameters] = EELMetamodel(dimension, numStartPoints, initDesign(IDChoice), xLow, xHigh, func, thetaLow, thetaHigh, sampStrat(SSChoice), regrPoly(RPChoice));
                     Output.ys = [Output.ys ys];
                     Output.dmodel = [Output.dmodel dmodel];
                     Output.parameters = [Output.parameters parameters];
+                    Output.error = [Output.error tempStruct];
                   catch MatlabError
                     tempStruct = struct('initDesign', initDesign(IDChoice), 'sampStrat', sampStrat(SSChoice), 'regrPoly', regrPoly(RPChoice), 'message', string(MatlabError.message));
+                    Output.ys = [Output.ys tempYs];
+                    Output.parameters = [Output.parameters tempParameters];
+                    Output.dmodel = [Output.dmodel tempDModel];
                     Output.error = [Output.error tempStruct];
                   end
               end %RPChoice Loop
